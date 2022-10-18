f4f15ece4d343b9069039b37bcb109bf985c5f3d
serhii <rootfellenua@gmail.com>
2022-10-18 12:17:08 -0700

    display active dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./Dialogs.module.scss";
+import { onSnapshot, doc } from "firebase/firestore";
+import { AuthContext } from "../../context/AuthContext";
+import { db } from "../../firebase";
 
 const Dialogs = () => {
+  const [dialogs, setDialogs] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getDialogs = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setDialogs(doc.data());
+      });
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getDialogs();
+  }, [currentUser.uid]);
+  console.log(dialogs);
   return (
     <div className={styles.wrapper}>
-      <div className={styles.userChat}>
-        <img className={styles.userChatImg} />
-        <div className={styles.userChatInfo}>
-          <span>Jane</span>
-          <p>Hello</p>
+      {Object.entries(dialogs)?.map((dialog) => (
+        <div className={styles.userChat} key={dialog[0]}>
+          <img
+            className={styles.userChatImg}
+            src={dialog[1].userInfo.photoURL}
+          />
+          <div className={styles.userChatInfo}>
+            <span>{dialog[1].userInfo.displayName}</span>
+            <p>{dialog[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.userChat}>
-        <img className={styles.userChatImg} />
-        <div className={styles.userChatInfo}>
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,9 +1,18 @@
-import React, { useContext } from "react";
-import styles from "./SearchInput.module.scss";
-import { useState } from "react";
-import { query, collection, where, getDocs, setDoc } from "firebase/firestore";
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
 import { db } from "../../firebase/index";
 import { AuthContext } from "../../context/AuthContext";
+import styles from "./SearchInput.module.scss";
 const SearchInput = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
@@ -27,21 +36,46 @@ const SearchInput = () => {
     }
   };
 
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   const handleSelect = async () => {
+    //check whether the group(chats in firestore) exists, if not create
     const combinedId =
       currentUser.uid > user.uid
         ? currentUser.uid + user.uid
         : user.uid + currentUser.uid;
     try {
-      const res = await getDocs(db, "chats", combinedId);
-      if (!res.exists()) {
-        await setDoc(doc(db, "chats", combinedId), { messages: [] });
-      }
-    } catch (error) {}
-  };
+      const res = await getDoc(doc(db, "chats", combinedId));
 
-  const handleKey = (e) => {
-    e.code === "Enter" && handleSearch();
+      if (!res.exists()) {
+        //create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
+
+    setUser(null);
+    setUsername("");
   };
   return (
     <div className={styles.wrapper}>
@@ -51,6 +85,7 @@ const SearchInput = () => {
           onChange={(e) => setUsername(e.target.value)}
           type="text"
           placeholder="Find a user"
+          value={username}
         />
       </div>
       {error && <span>User not found!</span>}
